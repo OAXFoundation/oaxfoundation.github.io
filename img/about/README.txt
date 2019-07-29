9289f0df40e769707c3031b2266d5cb5a783ba96
Wayland Chan <wayland@oax.org>
2019-07-29 14:33:33 +0800

    remove unused carousel, add to timeline

---
+++ img/about/README.txt
@@ -0,0 +1,1 @@
+These images are used in the timeline and need to be of size 200x200 resolution 300 to be displayed correctly
--- js/main.js
+++ js/main.js
@@ -3,53 +3,12 @@
 $(document).ready(function(){
 
 	$("#portfolio-contant-active").mixItUp();
-	// var mixer = mixItUp('.portfolio-contant-active', {
-  //   controls: {
-  //       toggleDefault: '.Founder'
-  //   }
-	// });
 
-	// $("#testimonial-slider").owlCarousel({
-	//     paginationSpeed : 500,      
-	//     singleItem:true,
-	//     autoPlay: 3000,
-	// });
-
-
-
-
-	// $("#clients-logo").owlCarousel({
-	// 	autoPlay: 3000,
-	// 	items : 5,
-	// 	itemsDesktop : [1199,5],
-	// 	itemsDesktopSmall : [979,5],
-	// });
-
-	// $("#works-logo").owlCarousel({
-	// 	autoPlay: 3000,
-	// 	items : 5,
-	// 	itemsDesktop : [1199,5],
-	// 	itemsDesktopSmall : [979,5],
-	// });
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
+	$( '#navbar-header .navbar-nav a' ).on( 'click', function () {
+		console.log('nav clicked');
+		$( '#navbar-header .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+		$( this ).parent( 'li' ).addClass( 'active' );
+	});
 
 });
 
