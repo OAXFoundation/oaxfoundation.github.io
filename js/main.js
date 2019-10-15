c6e9cbfac70f321e2ca93e7a2d834d51ec620e89
jiindong <jefferyng@openmindsresources.com>
2019-10-15 19:22:38 +0800

    20191015 changes. Including Team info, Homepage layout and timeline, new Resources page.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,32 @@ $(document).ready(function(){
 
 	/* OpenMinds Customization Starts */
 
+	// Homepage Timeline
+	$('#home-timeline').horizontalTimeline({
+		desktopDateIntervals: 300, 
+		tabletDateIntervals: 150, 
+		mobileDateIntervals: 120, 
+		minimalFirstDateInterval: true,  
+		dateDisplay: "monthYear", // dateTime, date, time, dayMonth, monthYear, year 
+		dateOrder: "normal", // normal, reverse  
+		autoplay: false, 
+		autoplaySpeed: 8, 
+		autoplayPause_onHover: false,   
+		useScrollWheel: false, 
+		useTouchSwipe: true, 
+		useKeyboardKeys: false, 
+		addRequiredFile: true, 
+		useFontAwesomeIcons: true, 
+		useNavBtns: true, 
+		useScrollBtns: false,  
+		iconBaseClass: "fas fa-3x", 
+		scrollLeft_iconClass: "", 
+		scrollRight_iconClass: "", 
+		prev_iconClass: "fa-chevron-left", 
+		next_iconClass: "fa-chevron-right", 
+		pause_iconClass: "fa-pause-circle", 
+		play_iconClass: "fa-play-circle"
+	});
 	
 
 	/* OpenMinds Customization Ends */
